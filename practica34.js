cedb7a860e26735c49a1b2a84b0cffa62014fa73
alfredo0983 <astro1230vsastro1230@.com>
2020-05-27 11:11:54 -0500

    first commit

---
+++ practica34.js
@@ -0,0 +1,22 @@
+ var nombre = document.getElementById('nombre');
+ var password = document.getElementById('password');
+ var error = document.getElementById('error');
+ error.style.color= 'red';
+ 
+function enviarFormulario(){
+    console.log ('enviando formulario...');
+    
+    var mensajesError= [];
+
+    if(nombre.value == null || nombre.value == '' ){
+        mensajesError.push('ingresa tu nombre');
+    }
+
+    if (password.value == null || password.value == '' ){
+        mensajesError.push('ingresa tu password');
+    }
+
+     error.innerHTML = mensajesError.join(', ');
+
+    return false;
+}
